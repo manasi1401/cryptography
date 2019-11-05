60a0a641a1af0b48df56fdb4718940300fc3204f
Manasi Paste <manasi.paste@mines.sdsmt.edu>
2019-11-05 00:06:16 -0700

    frequency analyzer

---
+++ frequency.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def get_text():
+    filename = input("Enter filename: ")
+    f = open(filename, "r")
+    text = f.read()
+
+    return text.upper()
+
+
+def analyze(text):
+    freq = np.zeros(26)
+
+    for c in text:
+        index = ord(c) - 65
+        if 0 <= index < 26:
+            freq[index] += 1
+
+    sum = freq.sum()
+    for i in range(26):
+        freq[i] /= sum
+    return freq
+
+
+def print_analysis(freq):
+    for i in range(26):
+        print(chr(i+65), " : ", freq[i])
+
+    sortedFreq = np.argsort(freq)
+    print("Max #1 : ", chr(sortedFreq[-1] + 65), " ", freq[sortedFreq[-1]])
+    print("Max #2 : ", chr(sortedFreq[-2] + 65), " ", freq[sortedFreq[-2]])
+    return freq[sortedFreq[-1]], freq[sortedFreq[-2]], freq[sortedFreq[-3]]
+
+
+text = get_text()
+freq = analyze(text)
+print_analysis(freq)
